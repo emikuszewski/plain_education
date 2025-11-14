b1b661cc2a356e1ebaec6d7a3c48d9164374da6d
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-11-14 10:50:11 -0600

    Initialize React application with APIPoliciesDemo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import APIPoliciesDemo from './api-policies';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <APIPoliciesDemo />
+  </React.StrictMode>
+);
